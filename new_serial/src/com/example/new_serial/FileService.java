dbf675a83b8e35fba75fd66245bb63519831f6c8
sichengliu-duke <sichengliu1992@gmail.com>
2015-10-31 23:06:42 -0400

    fileService

--- new_serial/src/com/example/new_serial/FileService.java
+++ new_serial/src/com/example/new_serial/FileService.java
@@ -18,29 +18,13 @@ public class FileService {
         super();  
         this.context = context;  
     }  
-    /**  
-     * 保存文件  
-     * @param fileName 文件名称  
-     * @param content  文件内容  
-     * @throws IOException  
-     */  
 
-    public void save(String fileName, String content) throws IOException {  
-        //以私有方式读写数据，创建出来的文件只能被该应用访问  
-        FileOutputStream fileOutputStream = context.openFileOutput(fileName, Context.MODE_WORLD_READABLE);  
-        fileOutputStream.write(content.getBytes());  
-        fileOutputStream.close();  
-    }  
+    //---------------------------------------------------------------------
+    //save the message in the textview to SD card
       
-    /**  
-     * 保存文件  
-     * @param fileName 文件名称  
-     * @param content  文件内容  
-     * @throws IOException  
-     */  
+
     public void saveToSDCard(String fileName, String content) throws IOException {  
-        //File file = new File(new File("/mnt/sdcard"),fileName);  
-        //考虑不同版本的sdCard目录不同，采用系统提供的API获取SD卡的目录  
+
         Calendar c = Calendar.getInstance();
 
         int hour = c.get(Calendar.HOUR_OF_DAY);
